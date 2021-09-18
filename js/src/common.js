75dee3aececd5d124ba4d011d2ebd73253b9660e
ICDL admin <icdl.webmaster.0001@gmail.com>
2021-09-18 19:55:39 +0900

    test

---
+++ js/src/common.js
@@ -0,0 +1,7 @@
+$(function() {
+	var height=$("header").height();
+	$(".site-content").css("margin-top", height);//10pxだけ余裕をもたせる
+
+  var width=$("aside").width();
+	$(".cd-content-wrapper").css("margin-left", width);//10pxだけ余裕をもたせる
+});
