be5e5a23fac744aef34b2d9087bf603a16db20bd
ICDL admin <icdl.webmaster.0001@gmail.com>
2021-10-06 18:12:54 +0900

    test
    tet

--- js/src/app.js
+++
@@ -1,9 +0,0 @@
-$('.lecutre-recommended-list').slick({
-  lazyLoad: 'ondemand',
-  slidesToShow: 4.5,
-  slidesToScroll: 4,
-  dots: false,
-  arrows: false,
-  infinite: false,
-  centerMode: false,
-});
