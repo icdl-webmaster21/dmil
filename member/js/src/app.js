bcb933f9e5a7423849d6a621af7d8a9323e7e725
ICDL admin <icdl.webmaster.0001@gmail.com>
2021-10-06 18:10:05 +0900

    new

---
+++ member/js/src/app.js
@@ -0,0 +1,9 @@
+$('.lecutre-recommended-list').slick({
+  lazyLoad: 'ondemand',
+  slidesToShow: 4.5,
+  slidesToScroll: 4,
+  dots: false,
+  arrows: false,
+  infinite: false,
+  centerMode: false,
+});
